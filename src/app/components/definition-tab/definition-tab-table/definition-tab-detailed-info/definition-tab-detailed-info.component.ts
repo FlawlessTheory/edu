0ed594b5a95aa626bc3252f11b8de3271a8bd104
d.shkurinskij <d.shkurinskiy.sis@gmail.com>
2020-10-05 17:23:39 +0700

    added new component

---
+++ src/app/components/definition-tab/definition-tab-table/definition-tab-detailed-info/definition-tab-detailed-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-definition-tab-detailed-info',
+  templateUrl: './definition-tab-detailed-info.component.html',
+  styleUrls: ['./definition-tab-detailed-info.component.css']
+})
+export class DefinitionTabDetailedInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
